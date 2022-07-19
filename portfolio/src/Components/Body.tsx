5f136b7da33c0ae6f8f0d4e4dba8400888e9e3e4
Hugo Vasconcelos <hugo.silva08@hotmail.com>
2022-07-19 12:46:59 -0300

    fix - removing tailwindcss and postcss and testing ReactHooks

--- portfolio/src/Components/Body.tsx
+++ portfolio/src/Components/Body.tsx
@@ -1,7 +1,11 @@
-export function Body () {
-    return (
-        <div className="flex-1">
-            Video
-        </div>
-    )
+import { useState } from "react";
+
+export function Body() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="flex-1">
+      <span>{count}</span>
+      <button onClick={() => setCount(count + 1)}></button>
+    </div>
+  );
 }
