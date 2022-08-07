d09356eccf707db8c6855ddefb4c11d683ffa32d
Hugo Vasconcelos <hugo.silva08@hotmail.com>
2022-08-07 16:28:02 -0300

    fix - fixing header component in Tests page

--- src/Pages/Tests/index.tsx
+++ src/Pages/Tests/index.tsx
@@ -9,9 +9,9 @@ export function Tests() {
   const [opened, setOpened] = useState(true);
   return (
     <StyledDiv>
-      <Header />
+      <Header opened={opened} setOpened={setOpened} />
       <StyledMain>
-        <TestsBody opened={opened} setOpened={setOpened} />
+        <TestsBody />
         <Collapse isOpened={opened}>
           <Sidebar />
         </Collapse>
